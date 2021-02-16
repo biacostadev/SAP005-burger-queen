8662b62b273a249dd97dbe9163dcae14b469fbbd
Nayara.Fugii <nayarac.neto@gmail.com>
2021-02-16 15:39:33 -0300

    realização do filter para breakfast

--- src/components/BtnClick.jsx
+++ src/components/BtnClick.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class BtnClick extends React.Component {
   constructor(props) {
     super(props);
--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ShowMenuCafe from "../components/ShowMenuCafe";
 import ItensPedidos from "../components/ItensPedido";
 import './Cafe.css';
 
 function Cafe() {
+  useEffect(() => {
+    getProducts()
+  },[])
 
+  const [menu, setMenu] = useState('');
+  const token=localStorage.getItem("token")
+
+  const getProducts = () => {
+    fetch('https://lab-api-bq.herokuapp.com/products',{
+      headers:{ 
+        "accept": "application/json",
+      "Authorization":`${token}`},
+    })
+    .then((response)=> response.json())
+    .then((json)=>{
+      const breakfast = json.filter(item => item.type === 'breakfast')
+      setMenu(breakfast)
+    })
+  }
+  
     return (
     <div className="Cafe">
-      <ShowMenuCafe />
+      <ShowMenuCafe Menu={ menu }/>
       <ItensPedidos />
     </div>
   );
